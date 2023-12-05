5474f763463e81d10c82579a73dffa53d2a90d3b
AntonioSoto01 <asotocid@gmail.com>
2023-12-05 10:55:55 +0100

    Creados perfiles de configuracion, en los que se establece la ruta del frontend

--- src/main/java/com/antonio/hundirlaflota/Controladores/JuegoController.java
+++ src/main/java/com/antonio/hundirlaflota/Controladores/JuegoController.java
@@ -3,6 +3,7 @@ package com.antonio.hundirlaflota.Controladores;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,9 +18,12 @@ import com.antonio.hundirlaflota.Servicios.JuegoService;
 
 @RestController
 @RequestMapping("/api/juego")
-@CrossOrigin(origins = "http://localhost:4200")
-public class JuegoController {
 
+@CrossOrigin(origins = "${frontend.url}")
+
+public class JuegoController {
+    @Value("${frontend.url}")
+    private String frontendUrl;
     @Autowired
     private JuegoService juegoService;
 
@@ -45,7 +49,7 @@ public class JuegoController {
     public ResponseEntity<ResultadoTurno> realizarTurno(@RequestParam String casilla) {
         Jugador jugadorActual = juegoService.getJugadorPorId((long) 1);
         ResultadoTurno resultadoTurno = juegoService.realizarTurno(jugadorActual, casilla);
-        System.out.println("id siguiente jugador "+resultadoTurno.getNombreJugador());
+        System.out.println("id siguiente jugador " + resultadoTurno.getNombreJugador());
         if (resultadoTurno.isError()) {
             return ResponseEntity.badRequest().body(resultadoTurno);
         } else {
--- src/main/java/com/antonio/hundirlaflota/Controladores/JugadorController.java
+++ src/main/java/com/antonio/hundirlaflota/Controladores/JugadorController.java
@@ -1,7 +1,9 @@
 package com.antonio.hundirlaflota.Controladores;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,17 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.antonio.hundirlaflota.Modelos.Jugador;
 import com.antonio.hundirlaflota.Repositorios.JugadorRepository;
+
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "${frontend.url}")
 public class JugadorController {
-	@Autowired
-private JugadorRepository jugadorRepository;
-@GetMapping("/jugadores")
+    @Value("${frontend.url}")
+private String frontendUrl;
+    @Autowired
+    private JugadorRepository jugadorRepository;
+
+    @GetMapping("/jugadores")
     public List<Jugador> getJugadores() {
         return (List<Jugador>) jugadorRepository.findAll();
     }
+
     @PostMapping("/jugadores")
     void addPlayer(@RequestBody Jugador jugador) {
-    	jugadorRepository.save(jugador);
+        jugadorRepository.save(jugador);
     }
 }
